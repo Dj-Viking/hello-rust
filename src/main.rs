e9894bc441c5b83073473e1b1d35d9db305035c6
dj-viking <anders.swedishviking@gmail.com>
2022-05-31 13:33:06 -0400

    hello ferris

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter, Stdout, StdoutLock};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout: Stdout = stdout();
+    let message: String = String::from("Hello fellow Rustaceans!");
+    let width: usize = message.chars().count();
+
+    let mut writer: BufWriter<StdoutLock> = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
